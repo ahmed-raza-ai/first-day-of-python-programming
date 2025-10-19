294ca607efbaa5b4c6e6248e92d248edede33814
MUHAMMAD AHMED RAZA <232654367+ahmed-raza-ai@users.noreply.github.com>
2025-10-19 10:45:39 -0700

    Add files via upload

---
+++ FIRST DAY OF MY PYTHON CODE.py
@@ -0,0 +1,33 @@
+# PRINT ANY STATEMENT
+print("welcome to beginners")
+# INTIALIZE THE VARIABLE
+name = "AHMED"
+name2 = "RAZA"
+age =18
+price = 100.1
+
+print("THE NAME OF THE STUDENT IS",name,"\n" "and the age of the student is",age)
+
+print("THE PRICE OF THE WATER BOTTLE IS" ,price)
+# how to check the data type of the variables
+print(type(name))
+print(type(age))
+print(type(price))
+
+
+#STRING CAN OPERATE WITH INTEGERS *
+print(name * 10)
+#STRING AND STRING CAN OPERATE WITH +
+print(name+name2)
+
+NUM1 = 12
+NUM2 = 45
+SUM = NUM1 + NUM2
+
+print("the sum of num1 and num2 is " ,SUM)
+SUM = NUM1 - NUM2
+print ("the diffrence of num1 and num2 is" , SUM)
+SUM = NUM1/NUM2
+print("the DIVISION of num1 and num2 is " ,SUM)
+SUM = NUM1*NUM2
+print("the MULTIPLICATION of num1 and num2 is " ,SUM)
