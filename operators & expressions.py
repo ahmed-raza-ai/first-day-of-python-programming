a755175c59847eceba9d6a394e97f00d54895636
MUHAMMAD AHMED RAZA <232654367+ahmed-raza-ai@users.noreply.github.com>
2026-01-10 18:41:17 +0500

    Add files via upload

---
+++ operators & expressions.py
@@ -0,0 +1,54 @@
+print("right now we can calculate marks of every students and find out the average" )
+ahmed = 45
+zeshaan = 70
+ali ,a = 90 ,3
+totalmarks = ahmed + zeshaan + ali 
+print("total marks of all student" , totalmarks)
+averageofstuentsmarks = totalmarks / a
+print("average of student marks in class" ,averageofstuentsmarks)
+
+
+randomnumber = int(input("enter any number"))
+num1 ,num2 = 3 ,5
+if randomnumber % num1 == 0 and randomnumber % num2 == 0:
+  print("the number is divisible by both")
+else:
+  print("the number is not divisible by both")
+
+number =int(input("inter any integer number"))  
+if number >= 10 and number <= 50:
+  print("number with in range")
+else:
+    print("number out of range")  
+
+
+print("now we can swap to number without using third variables")
+a,b =10 ,20
+a,b = b,a
+print("firstnumber"  ,a) 
+print("secondnumber"  ,b) 
+
+print("now we can make a program where shopkeper give discount if i wil purcase glossary more than  500 rupees")
+
+jamapple = jamorange = jammango = 200
+bun = 20
+butter = 200
+pancake = 250
+bread = 110
+
+totalamount = bun*10 +butter + jamapple
+if totalamount >= 500:
+   discount = totalamount /100 * 90
+   print("the total amount is" ,totalamount,"but we give you 10% disscount and your final amount will be ", discount)
+else:
+    print("totalamount " , totalamount)
+
+Totalamount = 200
+if Totalamount >= 500:
+   discount = Totalamount /100 * 90
+   print("the total amount is" ,Totalamount,"but we give you 10% disscount and your final amount will be ", discount)
+else:
+    print("totalamount " , Totalamount)
+
+
+
